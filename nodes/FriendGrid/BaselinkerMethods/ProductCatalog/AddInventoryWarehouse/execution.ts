49f8dfb9ff6c9d71a5b304a391f486760c890dc3
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-10-20 12:02:05 +0200

    fix input types and method for AddInventoryWarehouse execution

--- nodes/FriendGrid/BaselinkerMethods/ProductCatalog/AddInventoryWarehouse/execution.ts
+++ nodes/FriendGrid/BaselinkerMethods/ProductCatalog/AddInventoryWarehouse/execution.ts
@@ -3,15 +3,15 @@ import {makeBaselinkerRequest} from "../../request.maker";
 export async function addInventoryWarehouseExecution(args: {
 	apiKey: string;
 	input: {
-		"warehouse_id"?: number;
+		"warehouse_id"?: number | null;
 		"name": string;
-		"description"?: string;
-		"stock_edition"?: boolean;
+		"description": string;
+		"stock_edition": boolean;
 	};
 }) {
 	return await makeBaselinkerRequest({
 		apiKey: args.apiKey,
-		methodName: 'addInventoryPriceGroup',
+		methodName: 'addInventoryWarehouse',
 		input: args.input
 	});
 }
