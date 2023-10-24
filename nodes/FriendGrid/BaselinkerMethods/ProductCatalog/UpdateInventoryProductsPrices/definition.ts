49814907a967ee636c1c9fc6d6ee2b5ea5aafdb6
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-10-24 21:32:04 +0200

    add definition file for UI inputs in ProductCatalog API/UpdateInventoryProductsPrices

---
+++ nodes/FriendGrid/BaselinkerMethods/ProductCatalog/UpdateInventoryProductsPrices/definition.ts
@@ -0,0 +1,95 @@
+import {INodeProperties} from "n8n-workflow";
+import {Category, ProductCatalogMethod} from "../../types";
+
+export const updateInventoryProductsPricesDefinition: INodeProperties[] = [
+	{
+		displayName: 'Inventory ID',
+		name: 'inventory_id',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				category: [
+					Category.ProductCatalog
+				],
+				operation: [
+					ProductCatalogMethod.UpdateInventoryProductsPrices
+				],
+			},
+		},
+		default: '',
+		placeholder: '307',
+		description:'Catalog ID. The list of identifiers can be retrieved using the method getInventories.',
+	},
+	{
+		displayName: 'Products',
+		name: 'products',
+		placeholder: 'Add Product',
+		type: 'fixedCollection',
+		required: true,
+		default: {},
+		typeOptions: {
+			multipleValues: true,
+		},
+		description: 'An array of products, where the key is a product ID and the value is a list of prices. When determining the variant price, provide variant ID as a product ID.\n' +
+			'In the prices array the key should be the price group ID and the value - price for that price group. The list of price groups can be retrieved using the getInventoryPriceGroups method.',
+		options: [
+			{
+				name: 'metadataValues',
+				displayName: 'Metadata',
+				values: [
+					{
+						displayName: 'Product ID',
+						name: 'name',
+						type: 'string',
+						default: '',
+						description: 'An array of products, where the key is a product ID and the value is a list of stocks. When determining the variant stock, provide variant ID as a product ID.',
+						placeholder: '2685'
+					},
+					{
+						displayName: 'List of Prices',
+						name: 'value',
+						type: 'fixedCollection',
+						default: {},
+						description: 'In the prices array the key should be the price group ID and the value - price for that price group. The list of price groups can be retrieved using the getInventoryPriceGroups method.',
+						options: [
+							{
+								name: 'metadataValues',
+								displayName: 'Metadata',
+								values: [
+									{
+										displayName: 'Price Group ID',
+										name: 'price_group_id',
+										type: 'string',
+										default: '',
+										placeholder: '105'
+									},
+									{
+										displayName: 'Price',
+										name: 'price',
+										type: 'number',
+										typeOptions: {
+											numberPrecision: 2,
+										},
+										default: 0,
+										placeholder: '21.99'
+									},
+								]
+							}
+						]
+					},
+				],
+			},
+		],
+		displayOptions: {
+			show: {
+				category: [
+					Category.ProductCatalog
+				],
+				operation: [
+					ProductCatalogMethod.UpdateInventoryProductsPrices
+				],
+			},
+		},
+	},
+]
