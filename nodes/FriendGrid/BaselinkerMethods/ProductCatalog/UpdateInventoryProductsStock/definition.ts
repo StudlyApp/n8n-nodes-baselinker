f6e1e019c14d9c6c332a77250348aa554320d8b6
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-10-24 21:32:13 +0200

    add definition file for UI inputs in ProductCatalog API/UpdateInventoryProductsStock

---
+++ nodes/FriendGrid/BaselinkerMethods/ProductCatalog/UpdateInventoryProductsStock/definition.ts
@@ -0,0 +1,96 @@
+import {INodeProperties} from "n8n-workflow";
+import {Category, ProductCatalogMethod} from "../../types";
+
+export const updateInventoryProductsStockDefinition: INodeProperties[] = [
+	{
+		displayName: 'Inventory ID',
+		name: 'inventory_id',
+		type: 'string',
+		required: true,
+		displayOptions: {
+			show: {
+				category: [
+					Category.ProductCatalog
+				],
+				operation: [
+					ProductCatalogMethod.UpdateInventoryProductsStock
+				],
+			},
+		},
+		default: '',
+		placeholder: '307',
+		description:'Catalog ID. The list of identifiers can be retrieved using the method getInventories.',
+	},
+	{
+		displayName: 'Products',
+		name: 'products',
+		placeholder: 'Add Product',
+		type: 'fixedCollection',
+		required: true,
+		default: {},
+		typeOptions: {
+			multipleValues: true,
+		},
+		description: 'An array of products, where the key is a product ID and the value is a list of stocks. When determining the variant stock, provide variant ID as a product ID.\n' +
+			'In the stocks array the key should be the warehouse ID and the value - stock for that warehouse. The format of the warehouse identifier should be as follows: "[type:bl|shop|warehouse]_[id:int]". (e.g. "bl_123"). The list of warehouse identifiers can be retrieved using the getInventoryWarehousesmethod.\n' +
+			'\n' +
+			'Stocks cannot be assigned to the warehouses created automatically for purposes of keeping external stocks (shops, wholesalers, etc.).',
+		options: [
+			{
+				name: 'metadataValues',
+				displayName: 'Metadata',
+				values: [
+					{
+						displayName: 'Product ID',
+						name: 'name',
+						type: 'string',
+						default: '',
+						description: 'An array of products, where the key is a product ID and the value is a list of stocks. When determining the variant stock, provide variant ID as a product ID.',
+						placeholder: '2685'
+					},
+					{
+						displayName: 'Product ID and Variant ID for External Warehouse',
+						name: 'value',
+						type: 'fixedCollection',
+						default: {},
+						description: 'The value is an array containing the fields listed below',
+						options: [
+							{
+								name: 'metadataValues',
+								displayName: 'Metadata',
+								values: [
+									{
+										displayName: 'Warehouse ID',
+										name: 'warehouse_id',
+										type: 'string',
+										default: '',
+										description: 'In the stocks array the key should be the warehouse ID and the value - stock for that warehouse. The format of the warehouse identifier should be as follows: "[type:bl|shop|warehouse]_[ID:int]". (e.g. "bl_123"). The list of warehouse identifiers can be retrieved using the getInventoryWarehousesmethod.',
+										placeholder: 'bl_206'
+									},
+									{
+										displayName: 'Stock for that Warehouse',
+										name: 'stock',
+										type: 'number',
+										default: 0,
+										description: 'In the stocks array the key should be the warehouse ID and the value - stock for that warehouse. The format of the warehouse identifier should be as follows: "[type:bl|shop|warehouse]_[ID:int]". (e.g. "bl_123"). The list of warehouse identifiers can be retrieved using the getInventoryWarehousesmethod.',
+										placeholder: '3'
+									},
+								]
+							}
+						]
+					},
+				],
+			},
+		],
+		displayOptions: {
+			show: {
+				category: [
+					Category.ProductCatalog
+				],
+				operation: [
+					ProductCatalogMethod.UpdateInventoryProductsStock
+				],
+			},
+		},
+	},
+]
