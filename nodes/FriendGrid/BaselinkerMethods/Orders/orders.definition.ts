aa27bc5dfecefc8846af6c1756d58f42862ff36f
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-10-27 18:03:13 +0200

    feat(options): added new options for AddOrder and GetOrderSources

--- nodes/FriendGrid/BaselinkerMethods/Orders/orders.definition.ts
+++ nodes/FriendGrid/BaselinkerMethods/Orders/orders.definition.ts
@@ -1,6 +1,8 @@
 import {INodeProperties} from "n8n-workflow";
 import {Category, OrdersMethod} from "../types";
 import {getJournalListDefinition} from "./GetJournalList/definition";
+import {addOrderDefinition} from "./AddOrder/definition";
+import {getOrderSourcesDefinition} from "./GetOrderSources/definition";
 
 
 export const ordersDefinition: INodeProperties[] = [
@@ -23,10 +25,24 @@ export const ordersDefinition: INodeProperties[] = [
 				description: 'The method allows you to download a list of order events from the last 3 days',
 				action: 'Get journal list',
 			},
+			{
+				name: 'Add Order',
+				value: OrdersMethod.AddOrder,
+				description: 'The method allows adding a new order to the BaseLinker order manager',
+				action: 'Add order',
+			},
+			{
+				name: 'Get Order Sources',
+				value: OrdersMethod.GetOrderSources,
+				description: 'The method returns types of order sources along with their IDs. Order sources are grouped by their type that corresponds to a field order_source from the getOrders method. Available source types are "personal", "shop" or "marketplace_code" e.g. "ebay", "amazon", "ceneo", "emag", "allegro", etc.',
+				action: 'Get types of order sources along with their IDS',
+			},
 		],
 		// default: OrdersMethod.GetOrders.toString(),
 		default: '',
 		noDataExpression: true,
 	},
 	...getJournalListDefinition,
+	...addOrderDefinition,
+	...getOrderSourcesDefinition,
 ];
