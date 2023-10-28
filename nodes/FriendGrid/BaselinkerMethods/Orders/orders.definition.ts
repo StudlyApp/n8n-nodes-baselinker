6cfd83b57cc2b6de35e85268e0ee8ae9b0d12af9
Kamil Pietrzyk <kam.pietrz@gmail.com>
2023-10-28 18:36:14 +0200

    feat(options): added new options (options in body)
    -GetOrderExtraFields
    -GetOrders
    -GetOrderTransactionDetails
    -GetOrdersByEmail
    -GetOrdersByPhone
    -AddInvoice
    -GetInvoices
    -GetSeries
    -GetOrderStatusList
    -GetOrderPaymentsHistory
    -GetNewReceipts
    -GetReceipt
    -SetOrderFields
    -AddOrderProduct

--- nodes/FriendGrid/BaselinkerMethods/Orders/orders.definition.ts
+++ nodes/FriendGrid/BaselinkerMethods/Orders/orders.definition.ts
@@ -3,6 +3,20 @@ import {Category, OrdersMethod} from "../types";
 import {getJournalListDefinition} from "./GetJournalList/definition";
 import {addOrderDefinition} from "./AddOrder/definition";
 import {getOrderSourcesDefinition} from "./GetOrderSources/definition";
+import {getOrderExtraFieldsDefinition} from "./GetOrderExtraFields/definition";
+import {getOrdersDefinition} from "./GetOrders/definition";
+import {getOrderTransactionDetailsDefinition} from "./GetOrderTransactionDetails/definition";
+import {getOrdersByEmailDefinition} from "./GetOrdersByEmail/definition";
+import {getOrdersByPhoneDefinition} from "./GetOrdersByPhone/definition";
+import {addInvoiceDefinition} from "./AddInvoice/definition";
+import {getInvoicesDefinition} from "./GetInvoices/definition";
+import {getSeriesDefinition} from "./GetSeries/definition";
+import {getOrderStatusListDefinition} from "./GetOrderStatusList/definition";
+import {getOrderPaymentsHistoryDefinition} from "./GetOrderPaymentsHistory/definition";
+import {getNewReceiptsDefinition} from "./GetNewReceipts/definition";
+import {getReceiptDefinition} from "./GetReceipt/definition";
+import {setOrderFieldsDefinition} from "./SetOrderFields/definition";
+import {addOrderProductDefinition} from "./AddOrderProduct/definition";
 
 
 export const ordersDefinition: INodeProperties[] = [
@@ -23,7 +37,7 @@ export const ordersDefinition: INodeProperties[] = [
 				name: 'Get Journal List',
 				value: OrdersMethod.GetJournalList,
 				description: 'The method allows you to download a list of order events from the last 3 days',
-				action: 'Get journal list',
+				action: 'Gets journal list',
 			},
 			{
 				name: 'Add Order',
@@ -35,7 +49,102 @@ export const ordersDefinition: INodeProperties[] = [
 				name: 'Get Order Sources',
 				value: OrdersMethod.GetOrderSources,
 				description: 'The method returns types of order sources along with their IDs. Order sources are grouped by their type that corresponds to a field order_source from the getOrders method. Available source types are "personal", "shop" or "marketplace_code" e.g. "ebay", "amazon", "ceneo", "emag", "allegro", etc.',
-				action: 'Get types of order sources along with their IDS',
+				action: 'Gets types of order sources along with their IDS',
+			},
+			{
+				name: 'Get Order Extra Fields',
+				value: OrdersMethod.GetOrderExtraFields,
+				description: 'The method returns extra fields defined for the orders. Values of those fields can be set with method setOrderFields. In order to retrieve values of those fields set parameter include_custom_extra_fields in method getOrders',
+				action: 'Gets extra fields defined for the orders',
+			},
+			{
+				name: 'Get Orders',
+				value: OrdersMethod.GetOrders,
+				description: 'The method allows you to download orders from a specific date from the BaseLinker order manager. The order list can be limited using the filters described in the method parameters. A maximum of 100 orders are returned at a time.\n' +
+					'\n' +
+					'It is recommended to download only confirmed orders (get_unconfirmed_orders = false). Unconfirmed orders may be incomplete. The user may be, for example, in the process of creating an order - it already exists in the database, but not all information is completed. Unconfirmed orders may contain only a partial list of products and may be changed soon. Confirmed orders usually do not change anymore and can be safely downloaded to an external system.\n' +
+					'\n' +
+					'The best way to download the ongoing orders is:\n' +
+					'Collecting new order identifiers using getJournalList.\n' +
+					'\n' +
+					'Or, using this method:\n' +
+					'1. Setting the starting date and specifying it in the date_confirmed_from field\n' +
+					'2. Processing of all received orders. If 100 orders are received, there may be even more to download.\n' +
+					'3. Downloading the next package of orders by entering the value of the date_confirmed field from last downloaded order in the date_confirmed_from field. In order to avoid downloading the same orders value of date_confirmed should be increased by 1 second. This operation is repeated until you receive a package with less than 100 orders (this means that there are no more orders left to download).\n' +
+					'4. Saving the date_confirmed last processed order. You can download orders from this date onwards so that you do not download the same order twice. It is not possible for an order to \'jump\' into the database with an earlier confirmation date. This way you can be sure that all confirmed orders have been downloaded. ',
+				action: 'Get orders from a specific date from the order manager',
+			},
+			{
+				name: 'Get Order Transaction Details',
+				value: OrdersMethod.GetOrderTransactionDetails,
+				description: 'The method allows you to retrieve transaction details for a selected order (it now works only for orders from Amazon)',
+				action: 'Gets transaction details for a selected order',
+			},
+			{
+				name: 'Get Orders By Email',
+				value: OrdersMethod.GetOrdersByEmail,
+				description: 'The method allows to search for orders related to the given e-mail address. This function is designed to be used in plugins for mail clients (Thunderbird, Outlook, etc.).',
+				action: 'Gets orders related to the given email address',
+			},
+			{
+				name: 'Get Orders By Phone',
+				value: OrdersMethod.GetOrdersByPhone,
+				description: 'The method allows you to search for orders related to the given phone number. This function is intended for use in caller recognition programs.',
+				action: 'Gets orders related to the given phone number',
+			},
+			{
+				name: 'Add Invoice',
+				value: OrdersMethod.AddInvoice,
+				description: 'The method allows to issue an order invoice',
+				action: 'Add an order invoice',
+			},
+			{
+				name: 'Get Invoices',
+				value: OrdersMethod.GetInvoices,
+				description: 'The method allows you to download invoices issued from the BaseLinker order manager. The list of invoices can be limited using filters described in the method parameters. Maximum 100 invoices are returned at a time.',
+				action: 'Gets invoices from the order manager',
+			},
+			{
+				name: 'Get Series',
+				value: OrdersMethod.GetSeries,
+				description: 'The method allows to download a series of invoice/receipt numbering',
+				action: 'Gets series of invoice and receipt numbering',
+			},
+			{
+				name: 'Get Order Status List',
+				value: OrdersMethod.GetOrderStatusList,
+				description: 'The method allows you to download order statuses created by the customer in the BaseLinker order manager',
+				action: 'Gets order statues created by the customer in order manager',
+			},
+			{
+				name: 'Get Order Payment History',
+				value: OrdersMethod.GetOrderPaymentsHistory,
+				description: 'The method allows you to retrieve payment history for a selected order, including an external payment identifier from the payment gateway. One order can have multiple payment history entries, caused by surcharges, order value changes, manual payment editing.',
+				action: 'Gets payment history for a selected order',
+			},
+			{
+				name: 'Get New Receipts',
+				value: OrdersMethod.GetNewReceipts,
+				description: 'The method allows you to retrieve receipts waiting to be issued. This method should be used in creating integration with a fiscal printer. The method can be requested for new receipts every e.g. 10 seconds. If any receipts appear in response, they should be confirmed by the setOrderReceipt method after printing to disappear from the waiting list.',
+				action: 'Gets receipts waiting to be issued',
+			},
+			{
+				name: 'Get Receipt',
+				value: OrdersMethod.GetReceipt,
+				description: 'The method allows you to retrieve a single receipt from the BaseLinker order manager. To retrieve a list of new receipts (when integrating a fiscal printer), use the getNewReceipts method.',
+				action: 'Gets a single receipt from order manager',
+			},
+			{
+				name: 'Set Order Fields',
+				value: OrdersMethod.SetOrderFields,
+				description: 'The method allows you to edit selected fields (e.g. address data, notes, etc.) of a specific order. Only the fields that you want to edit should be given, other fields can be omitted in the request.',
+				action: 'Edit selected fields of a specific order',
+			},
+			{
+				name: 'Add Order Product',
+				value: OrdersMethod.AddOrderProduct,
+				description: 'The method allows you to add a new product to your order',
+				action: 'Add a new product to order',
 			},
 		],
 		// default: OrdersMethod.GetOrders.toString(),
@@ -45,4 +154,18 @@ export const ordersDefinition: INodeProperties[] = [
 	...getJournalListDefinition,
 	...addOrderDefinition,
 	...getOrderSourcesDefinition,
+	...getOrderExtraFieldsDefinition,
+	...getOrdersDefinition,
+	...getOrderTransactionDetailsDefinition,
+	...getOrdersByEmailDefinition,
+	...getOrdersByPhoneDefinition,
+	...addInvoiceDefinition,
+	...getInvoicesDefinition,
+	...getSeriesDefinition,
+	...getOrderStatusListDefinition,
+	...getOrderPaymentsHistoryDefinition,
+	...getNewReceiptsDefinition,
+	...getReceiptDefinition,
+	...setOrderFieldsDefinition,
+	...addOrderProductDefinition,
 ];
